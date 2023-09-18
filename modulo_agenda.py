a6609371d88646406e77b6e0f9e022b5cd47eb6b
erick023 <erickrenan023@gmail.com>
2023-09-17 22:53:24 -0300

    agenda

---
+++ modulo_agenda.py
@@ -0,0 +1,40 @@
+agenda = {}
+arquivo = open('agenda_texto.txt','at')
+
+def adiciona(nome, tel):
+    agenda[nome] = tel
+
+def mostraContato(nome):
+    if nome in agenda:
+        print(nome, end=' ')
+        print(agenda[nome])
+    else:
+        print('Contato não Encontrado!')
+
+
+def mostraLista():
+    if len(agenda) == 0:
+        print('Agenda Vazia!')
+    else:
+        for contato in agenda.items():
+            print(*contato)
+            
+
+def apagaContato(nome):
+    if nome in agenda:
+        agenda.pop(nome)
+    else:
+        print('Contato não encontrado')
+
+def apagaTodosContatos():
+    agenda.clear()
+
+def gravaAgenda():
+    if len(agenda) == 0:
+        print('Agenda vazia!')
+    else:
+      for contato in agenda.items():
+        contatoStr = ' '.join(map(str,contato))
+        arquivo.write(contatoStr)
+    print("Agenda Gravada!")
+    arquivo.close()
